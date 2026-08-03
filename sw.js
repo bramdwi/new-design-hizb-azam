ac33d5617ee50284460a4939bef6f502e9aa113f
Brahman Dwi Budiartomo <bramdwi@Brahmans-MacBook-Air.local>
2026-08-03 18:39:48 +0700

    style: update icon-512.png to full bleed PWA maskable icon

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy for 100% offline reading
    ============================================ */
 
-const CACHE_NAME = 'hizb-azam-v11';
+const CACHE_NAME = 'hizb-azam-v12';
 
 const PRECACHE_URLS = [
     './',
